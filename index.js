d8c76b072e42cd23943702a891a64c01c388b70e
hex-codes <hectorcardenas57@gmail.com>
2020-03-05 19:05:27 -0600

    Grid added w/ JS, CSS added

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-function createDivs(num) {
-    const mainDiv = document.querySelector('.container');
-    
-    for (let i = 0; i < num; i++) {
-        const createDiv = document.createElement('div');
-        createDiv.classList.add(i);
-        createDiv.textContent = 'First added div';
-        mainDiv.appendChild(createDiv);    
+//Declaration of main Div
+const mainDiv = document.querySelector('.container');
+
+
+
+
+//Creates 16 columns
+
+    for (let i = 0; i < 16; i++) {
+        const createRows = document.createElement('div');
+        createRows.classList.add(i);
+        //createRows.textContent = 'First added div';
+        mainDiv.appendChild(createRows);    
+        createRows.setAttribute('class','gridRow');
         
+    }    
+
+
+
+
+//Creates 16 rows 
+    const rows = document.querySelectorAll('.gridRow');
+
+    for (let j = 0; j < rows.length; j++) {
+           
+        for (let i = 0; i < 16; i++) {
+            const createColumns = document.createElement('div');
+            createColumns.classList.add(i);
+            rows[j].appendChild(createColumns);    
+            createColumns.setAttribute('class','gridColumn');
+        }
     }
+    
+
+
+//Current working on
+// function startHoverColorChange() {   
+//    const column = document.querySelector('body');
    
-}
-
-
+//    column.addEventListener('click',function(){
+//        this.style.backgroundColor = 'red';
+//    });
+   
+// }
