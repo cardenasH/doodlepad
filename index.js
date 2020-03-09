7443e2033f3eb4707c0c8e29f5e4548f9c2b8b75
hex-codes <hectorcardenas57@gmail.com>
2020-03-09 18:42:37 -0500

    working basic doodle pad

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-//Declaration of main Div
-const mainDiv = document.querySelector('.container');
-
-
-
-
-//Creates 16 columns
-
-    for (let i = 0; i < 16; i++) {
-        const createRows = document.createElement('div');
-        createRows.classList.add(i);
-        //createRows.textContent = 'First added div';
-        mainDiv.appendChild(createRows);    
-        createRows.setAttribute('class','gridRow');
-        
-    }    
-
-
-
-
-//Creates 16 rows 
-    const rows = document.querySelectorAll('.gridRow');
-
-    for (let j = 0; j < rows.length; j++) {
-           
+    //Declaration of main Div
+    const mainDiv = document.querySelector('.container');
+    
+    
+    
+    
+    //Creates 16 columns
+    function createColumns(){
         for (let i = 0; i < 16; i++) {
-            const createColumns = document.createElement('div');
-            createColumns.classList.add(i);
-            rows[j].appendChild(createColumns);    
-            createColumns.setAttribute('class','gridColumn');
+            const createRows = document.createElement('div');
+            createRows.classList.add(i);
+            //createRows.textContent = 'First added div';
+            mainDiv.appendChild(createRows);    
+            createRows.setAttribute('class','gridRow');
+            
+        }    
+    }
+    
+    
+    
+    
+    //Creates 16 rows 
+    function createRows(){
+        const rows = document.querySelectorAll('.gridRow');
+        
+        for (let j = 0; j < rows.length; j++) {
+            for (let i = 0; i < 16; i++) {
+                const createColumns = document.createElement('div');
+                createColumns.classList.add(i);
+                rows[j].appendChild(createColumns);    
+                createColumns.setAttribute('class','gridColumn');
+            }
         }
     }
     
-
-
-//Current working on
-// function startHoverColorChange() {   
-//    const column = document.querySelector('body');
-   
-//    column.addEventListener('click',function(){
-//        this.style.backgroundColor = 'red';
-//    });
-   
-// }
+    //Event Listener to change indiviual square colors
+    function changeColor(e) {
+        document.querySelectorAll('.gridColumn').forEach(item => {
+            item.addEventListener('mouseover', event => {
+                //handle click
+                item.style.backgroundColor = 'red';
+            })
+        })
+    }
+    
+//Initiates all functions
+function createGrid() {
+    createColumns();
+    createRows();
+    changeColor();
+}
